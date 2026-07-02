f2260e865559e88b89c333cf5cb12b0d312c8f70
sergiomagalhaesgit1305-dotcom <sergiomagalhaesgit1305@gmail.com>
2026-07-02 19:40:06 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,28 @@
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Select and submit a number rating
+- See the "Thank you" card state after submitting a rating
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+
+### What I learned
+
+Learned how to use forEach and how ti hide and display text
+
+
+### Continued development
+
+Better understanding of javascript
---
+++ description.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const questions = document.querySelectorAll(".faq-question");
+
+    questions.forEach(question => {
+        question.addEventListener("click", () => {
+            const answer = question.nextElementSibling;
+            const icon = question.querySelector(".icon");
+
+            if (answer.style.display == "block") {
+                answer.style.display = "none";
+
+                icon.innerHTML = `<img src="assets/images/icon-plus.svg" alt="Plus that sits to the right of the paragraph">`;
+            } else {
+                answer.style.display = "block";
+
+                icon.innerHTML = `<img src="assets/images/icon-minus.svg" alt="Minus that sits to the right of the paragraph">`
+            }
+        })
+    })
+})
